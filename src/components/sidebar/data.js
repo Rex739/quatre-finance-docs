c5352c4d8413524719c7b5d0c4b7f8657661a15f
Rex739 <ojemehprogress@gmail.com>
2023-03-20 13:52:24 +0100

    app update

--- src/components/sidebar/data.js
+++ src/components/sidebar/data.js
@@ -2,65 +2,81 @@ export const sidebarList = [
   {
     id: 0,
     navList: "What is a Digesu?",
+    href: "/",
   },
   {
     id: 1,
     navList: "About Digesu",
+    href: "/about-digesu",
   },
   {
     id: 2,
     navList: "Difference between Digesu & other apps",
+    href: "/difference-between-digesu-and-other-apps",
   },
   {
     id: 3,
     navList: "What is a Band?",
+    href: "/what-is-a-band",
   },
   {
     id: 4,
     navList: "Steps in a Band",
+    href: "/steps-in-a-band",
   },
   {
     id: 5,
     navList: "Quorum",
+    href: "/quorum",
   },
   {
     id: 6,
     navList: "Amount",
+    href: "/amount",
   },
   {
     id: 7,
     navList: "Duration",
+    href: "/duration",
   },
   {
     id: 8,
     navList: "Multiplier",
+    href: "/multiplier",
   },
   {
     id: 9,
     navList: "Liquidation",
+    href: "/what-is-a-digesu",
   },
   {
     id: 10,
     navList: "Public VS Private Band",
+    href: "/public-private-band",
   },
   {
     id: 11,
     navList: "Cycle",
+    href: "/cycle",
   },
   {
     id: 12,
     navList: "How to Get Finance",
+    href: "/how-to-get-finance",
   },
   {
     id: 13,
     navList: "How to Payback",
+    href: "/how-to-payback",
   },
   {
     id: 14,
     navList: "How to Claim dues",
+    href: "/how-to-claim-dues",
   },
   {
     id: 16,
     navList: "Testnet Info",
+    href: "/testnet-info",
   },
 ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react"
+import SideBar from "../components/sidebar/sidebar"
 // import SEOHead from "../components/SEOHead/SEOHead"
 // import TransparenLayer from "../components/transparentLayer"
 
-const Home = ({ isClicked }) => {
+const Home = ({ handleClick, isClicked }) => {
   return (
     <main className="bg-green1 relative lg:flex">
-     
       <main className="wrapper text-white1 lg:flex">
-       
+        <SideBar handleClick={handleClick} />
         <main></main>
       </main>
     </main>
