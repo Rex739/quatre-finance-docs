f2314ea1b1aee318514a94270cbae8ec7dca5636
Rex739 <ojemehprogress@gmail.com>
2023-03-20 23:17:55 +0100

    update

--- src/components/docs.js
+++ src/components/docs.js
@@ -2,14 +2,21 @@ import React from "react"
 
 const Docs = ({ heading, paragraph1, paragraph2, paragraph3, paragraph4 }) => {
   return (
-    <section>
-      <h1 className="font-bold text-xl lg:text-[2rem]">
-        {heading}
-      </h1>
-      <div className="">{paragraph1}</div>
-      <div>{paragraph2}</div>
-      <div>{paragraph3}</div>
-      <div>{paragraph4}</div>
+    <section className="py-10 lg:py-20 wrapper">
+      <div className="lg:bg-gray1 lg:px-5 lg:py-6">
+        <h1 className="font-bold text-xl lg:text-[2rem] mb-8 lg:mb-10">
+          {heading}
+        </h1>
+        <div className="mb-5">{paragraph1}</div>
+        <div className="mb-5">{paragraph2}</div>
+        <div className="mb-5">{paragraph3}</div>
+        <div className="mb-5">{paragraph4}</div>
+      </div>
+
+      <div>
+        <button></button>
+        <button></button>
+      </div>
     </section>
   )
 }
