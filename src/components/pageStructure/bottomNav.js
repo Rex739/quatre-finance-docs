f706f066327ea62f04eb52553c779f7dc3313512
Rex739 <ojemehprogress@gmail.com>
2023-03-21 02:24:46 +0100

    restructured bottom nav

--- src/components/pageStructure/bottomNav.js
+++ src/components/pageStructure/bottomNav.js
@@ -4,44 +4,44 @@ import React from "react"
 const BottomNav = ({ previous, next, prevLink, nextLink }) => {
   return (
     <section className="flex justify-between py-10 lg:py-20">
-      <div
-        className={[
-          "w-2/4  border border-border-gray rounded-md flex justify-start text-left px-2 mr-2 lg:mr-5 py-3 ",
-          !previous && "invisible",
-        ].join(" ")}
-      >
-        <Link href={"/" + prevLink}>
-          <a>
-            <span className="font-meduim text-xs">Previous</span>
-            <div className="flex justify-center">
+      <Link href={"/" + prevLink}>
+        <a
+          className={[
+            "w-2/4  border border-borderGray rounded-md flex justify-start text-left px-2 mr-2 lg:mr-5 py-3 ",
+            !previous && "invisible",
+          ].join(" ")}
+        >
+          <button className="flex flex-col">
+            <span className="font-meduim text-xs text-left mr-auto">Previous</span>
+            <div className="flex">
               <img src="images/prev.svg" alt="previous" />
               <span className="ml-[8px] text-xs lg:text-base font-meduim text-orange">
                 {previous}
               </span>
             </div>
-          </a>
-        </Link>
-      </div>
+          </button>
+        </a>
+      </Link>
 
       {next && (
-        <div
-          className={[
-            "w-2/4 border border-border-gray rounded-md flex justify-end text-right px-2 ml-2 lg:ml-5 py-3",
-            !next && "invisible",
-          ].join(" ")}
-        >
-          <Link href={"/" + nextLink}>
-            <a>
-              <span className="font-meduim text-xs">Next</span>
+        <Link href={"/" + nextLink}>
+          <a
+            className={[
+              "w-2/4  border border-borderGray rounded-md flex justify-end text-right px-2 ml-2 lg:ml-5 py-3 ",
+              !next && "invisible",
+            ].join(" ")}
+          >
+            <button className="flex flex-col">
+              <span className="font-meduim text-xs ml-auto">Next</span>
               <div className="flex">
-                <span className="mr-[8px] font-meduim text-orange text-xs lg:text-base">
+                <span className="mr-[8px] text-xs lg:text-base font-meduim text-orange">
                   {next}
                 </span>
                 <img src="images/next.svg" alt="next" />
               </div>
-            </a>
-          </Link>
-        </div>
+            </button>
+          </a>
+        </Link>
       )}
     </section>
   )
