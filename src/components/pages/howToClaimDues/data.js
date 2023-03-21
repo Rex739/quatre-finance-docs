3c790aa183f27db58da9089c4203ff08f667eb5b
Rex739 <ojemehprogress@gmail.com>
2023-03-21 22:10:13 +0100

    implemented 'how to claim' page

---
+++ src/components/pages/howToClaimDues/data.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import { howToPayback, testnetInfo } from "../../pageStructure/pageLinks"
+
+export const _claimDues = [
+  {
+    id: 0,
+    heading: "CLAIM DUES",
+    paragraph1: (
+      <p>
+        When all the particints have completed their turn and fully return
+        expected value to the pool, Any of the band members can the
+        COMPLETETHEROUND function which distributes their funds accordingly.
+        Thereafter, each member can claim their part.
+      </p>
+    ),
+
+    previous: howToPayback,
+    next: testnetInfo,
+  },
+]
