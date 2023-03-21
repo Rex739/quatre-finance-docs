6b6b66386778286c732ea0944013922df51b3a80
Rex739 <ojemehprogress@gmail.com>
2023-03-21 01:49:28 +0100

    fixed bug in page link component

--- src/components/pageStructure/docs.js
+++ src/components/pageStructure/docs.js
@@ -16,7 +16,7 @@ const Docs = ({
   return (
     <section className="py-10 lg:py-20 wrapper">
       <div className="lg:bg-gray1 lg:px-5 lg:py-6">
-        <h1 className="font-bold text-xl lg:text-[2rem] mb-8 lg:mb-10">
+        <h1 className="font-bold text-[2rem]  mb-8 lg:mb-10">
           {heading}
         </h1>
         <div className="mb-5">{paragraph1}</div>
