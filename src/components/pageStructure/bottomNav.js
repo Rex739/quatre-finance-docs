156c432d08894ddd2a56743b75c5397cef3d9e72
Rex739 <ojemehprogress@gmail.com>
2023-03-21 23:07:43 +0100

    added hover effect to bottom buttons

--- src/components/pageStructure/bottomNav.js
+++ src/components/pageStructure/bottomNav.js
@@ -9,10 +9,13 @@ const BottomNav = ({ previous, next, prevLink, nextLink }) => {
           className={[
             "w-2/4  border border-borderGray rounded-md flex justify-start text-left px-2 mr-2 lg:mr-5 py-3 ",
             !previous && "invisible",
+            "hover:border-orange",
           ].join(" ")}
         >
           <button className="flex flex-col">
-            <span className="font-meduim text-xs text-left mr-auto">Previous</span>
+            <span className="font-meduim text-xs text-left mr-auto">
+              Previous
+            </span>
             <div className="flex">
               <img src="images/prev.svg" alt="previous" />
               <span className="ml-[8px] text-xs lg:text-base font-meduim text-orange">
@@ -29,6 +32,7 @@ const BottomNav = ({ previous, next, prevLink, nextLink }) => {
             className={[
               "w-2/4  border border-borderGray rounded-md flex justify-end text-right px-2 ml-2 lg:ml-5 py-3 ",
               !next && "invisible",
+              "hover:border-orange",
             ].join(" ")}
           >
             <button className="flex flex-col">
