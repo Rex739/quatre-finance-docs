dd5ad1c173a286312f4a958de560fe91967985d5
Rex739 <ojemehprogress@gmail.com>
2023-03-21 14:53:14 +0100

    modified bottom nav

--- src/components/pageStructure/bottomNav.js
+++ src/components/pageStructure/bottomNav.js
@@ -4,7 +4,7 @@ import React from "react"
 const BottomNav = ({ previous, next, prevLink, nextLink }) => {
   return (
     <section className="flex justify-between py-10 lg:py-20">
-      <Link href={"/" + prevLink}>
+      <Link href={prevLink}>
         <a
           className={[
             "w-2/4  border border-borderGray rounded-md flex justify-start text-left px-2 mr-2 lg:mr-5 py-3 ",
@@ -12,7 +12,9 @@ const BottomNav = ({ previous, next, prevLink, nextLink }) => {
           ].join(" ")}
         >
           <button className="flex flex-col">
-            <span className="font-meduim text-xs text-left mr-auto">Previous</span>
+            <span className="font-meduim text-xs text-left mr-auto">
+              Previous
+            </span>
             <div className="flex">
               <img src="images/prev.svg" alt="previous" />
               <span className="ml-[8px] text-xs lg:text-base font-meduim text-orange">
@@ -24,7 +26,7 @@ const BottomNav = ({ previous, next, prevLink, nextLink }) => {
       </Link>
 
       {next && (
-        <Link href={"/" + nextLink}>
+        <Link href={nextLink}>
           <a
             className={[
               "w-2/4  border border-borderGray rounded-md flex justify-end text-right px-2 ml-2 lg:ml-5 py-3 ",
