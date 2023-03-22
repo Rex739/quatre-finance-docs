01be943499a3d4d5c24d5cc3291cde0effac636d
Rex739 <ojemehprogress@gmail.com>
2023-03-22 21:15:02 +0100

    modified 'difference between digesu and other apps'

--- src/components/pages/DigesuAndDapps/data.js
+++ src/components/pages/DigesuAndDapps/data.js
@@ -1,29 +1,46 @@
 import React from "react"
 
-import { aboutDigesu, whatIsABand, whatIsADigesu } from "../../pageStructure/pageLinks"
+import {
+  aboutDigesu,
+  whatIsABand,
+  whatIsADigesu,
+} from "../../pageStructure/pageLinks"
 
 export const _differnceBetweenDigesuAndOtherDapps = [
   {
     id: 0,
     heading: "DIFFERENCE BETWEEN DIGESU & OTHER APPS",
     paragraph1: (
-      <p>
-        You cannot create a new band with an existing amount unless the Quorum
-        for the selected amount is reached. That is, if a band already has an
-        amount of 1BNB and it is still active, no band with same amount can
-        exist simultaneously unless the existing band is filled up.
-      </p>
+      <div>
+        <ol typeof="A" className="list-inside list-[a]">
+          <li>It is 100% controlled by user.</li>
+          <li>QuatreFinance does not hold any fund in custody.</li>
+          <li>
+            <span>It is flexible: that is,</span>
+            <ul className="list-disc list-inside">
+              <li>
+                Use may create a band of many friends (i.e restricted band, with
+                participants not greater than 255) and rotate pooled fund among
+                participants until the cycle is completed or join an existing
+                band.
+              </li>
+              <li>Setting collateral (i.e multiplier) is optional.</li>
+              <li>
+                Settings such as collateral factor, are determined by user.
+              </li>
+            </ul>
+          </li>
+          <li>It is highly decentralized.</li>
+          <li>
+            Every participant in a band is a borrower and a lender at the same
+            time.
+          </li>
+          <li>Collateral is optional</li>
+          <li>Users earn from the platform. No interest is charged </li>
+        </ol>
+      </div>
     ),
-    paragraph2: (
-      <p>
-        This is to achieve efficiency and moderation. T here is a limit to the
-        number of bands you can join. You cannot create a band with duration (in
-        days) above 255 days. Setting APR below 100 will cause unexpected
-        behavior. 100 mean no interest should be charged in this band. 120 means
-        to set interest rate to 20% per annum. Same rule for APR applies to
-        multiplier (i.e. collateral factor)
-      </p>
-    ),
+
     previous: aboutDigesu,
     next: whatIsABand,
   },
